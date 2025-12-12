e30fc24373218d909313dfe405c9d7ae89bac260
CeilingG <42397975+Shalomguan@users.noreply.github.com>
2025-12-12 23:05:01 +0800

    更改时期

--- app/talks.ts
+++ app/talks.ts
@@ -3,7 +3,7 @@ import type { TalkItem } from '~/types/talk'
 export default [
   {
     text: '测试说说',
-    date: '2025-09-24 00:00',
+    date: '2025-12-12 23:04',
     tags: ['test'],
     location: '武汉'
   },
